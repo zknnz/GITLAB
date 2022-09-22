079fa1cbf238fcd5437deff797d0445d2e178b03
zmirxgez <kunanoon1506@gmail.com>
2022-09-22 15:34:08 +0700

    modified blood

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-  char Name[30][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
+  char Name[20][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
   int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306}; 
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
   int N = 6, i;
